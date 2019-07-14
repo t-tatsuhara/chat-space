97fa6f1334d47e273006d76916685603cd58f18e
t-tatsuhara <adgjmptw19890305@icloud.com>
2019-07-14 19:05:22 +0900

    message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,30 @@
 class MessagesController < ApplicationController
+
+    before_action :set_group, only: [:index, :create]
+
   def index
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
   end
+
+  def create
+    @message = Messages.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(@group), notice: 'メッセージの送信に失敗しました'
+    else
+      redirect_to group_messages_path(@group), alert: 'メッセージを入力してください'
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id,group_id: params[:group_id])
+  end
+  
+  
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
 
   has_many :members
   has_many :groups, through: :members
+  has_many :messages
 end
